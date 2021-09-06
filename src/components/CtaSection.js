1152fa8b93f83035e37142c74821cc68cd84f884
Stackbit Code Editor <projects@stackbit.com>
2021-09-06 08:58:00 +0000

    CtaSection.js: changed

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -57,7 +57,7 @@ export default class CtaSection extends React.Component {
                                     'text-right': alignX === 'right'
                                 })}
                             >
-                                {title && <h1 className="cta__title">{title}</h1>}
+                                {title &&  }
                                 {content && <div className="cta__copy">{markdownify(content)}</div>}
                             </div>
                         )}
