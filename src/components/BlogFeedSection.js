1a57400f6cd3af171c1f29ecd55350ca7c9bc4e0
Stackbit Code Editor <projects@stackbit.com>
2021-09-06 08:57:51 +0000

    BlogFeedSection.js: changed

--- src/components/BlogFeedSection.js
+++ src/components/BlogFeedSection.js
@@ -203,7 +203,7 @@ export default class BlogFeedSection extends React.Component {
                         })}
                     >
                         {subtitle && <div className="section__subtitle">{subtitle}</div>}
-                        {title && <div className="section__subtitle">{title}</div> }
+                        {title && <div className="section__title">{title}</div> }
                     </div>
                 )}
                 <div className="container">
